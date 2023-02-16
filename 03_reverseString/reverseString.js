b90e8b0937112c3c9a463ea1e6ea6ac218c6b401
nobalancemaybe <121553629+nobalancemaybe@users.noreply.github.com>
2023-02-16 12:26:13 +0100

    reversed a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(string) {  
+  return string.split('').reverse().join('')
+// .split will return the string into an array(split('') will return array of all characters)
+// .reverse will reverse an array
+// .join will join an array into a string (join('') will join the array without adding anything)
+}
 // Do not edit below this line
 module.exports = reverseString;
